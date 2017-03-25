ceb158044c742bf611b13a1ac93ed72f1574c3ae
m3nac3-2-s0c13ty <paramunix@gmail.com>
2017-03-25 23:46:16 +0800

    Added MacOSx POC

---
+++ Install-macOSX.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+ts=`date +%F-%R`
+
+easy_install pip
+pip install ansible
+pip install vagrant
+pip install virtualbox
+
+## Installing the vagrant Guest VM ###
+
+wget https://atlas.hashicorp.com/centos/boxes/7/versions/1702.01/providers/virtualbox.box
+vagrant up virtualbox.box --provider virtualbox
+
+
+echo "----------------------"
+echo
+
+
