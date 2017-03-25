67c55202d61a8cb652e5bab29aedd3a5983fe587
m3nac3-2-s0c13ty <paramunix@gmail.com>
2017-03-26 00:06:09 +0800

    Added vm build chk in Install script

--- Install-macOSX.sh
+++ Install-macOSX.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 ts=`date +%F-%R`
@@ -13,6 +13,17 @@ pip install virtualbox
 wget https://atlas.hashicorp.com/centos/boxes/7/versions/1702.01/providers/virtualbox.box
 vagrant up virtualbox.box --provider virtualbox
 
+function check {
+	check vagrant status|grep -i running
+	if [ $? -eq  0 ]
+		then
+  			vagrant provision
+		else
+  			sleep 10
+	fi
+}
+
+check
 
 echo "----------------------"
 echo
